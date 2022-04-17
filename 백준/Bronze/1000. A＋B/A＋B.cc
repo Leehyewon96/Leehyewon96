282bb5311e26de1d6badad3d4197fd41a653be85
prooni <lhw629@naver.com>
2022-04-17 16:12:59 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    // cout << "정수 A, B를 입력하세요(단, 0 < A, B < 10)" << endl;
+    cin >> A >> B;
+    cout << A+B << endl;
+    
+    return 0;
+}
