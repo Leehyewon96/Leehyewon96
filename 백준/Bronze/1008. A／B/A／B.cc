dbf988c946dfd5076462b12e0b32b9b4747ba70b
prooni <lhw629@naver.com>
2022-04-17 16:14:10 +0900

    [Bronze IV] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double A, B;
+ 
+    cin >> A >> B;
+    cout<<fixed;
+    cout.precision(15);
+    cout << A/B << endl;
+    
+    return 0;
+}
